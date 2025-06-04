3f3fbdc5e10822ed8b7315d434ba04895705f243
AdanUade <168041829+AdanUade@users.noreply.github.com>
2025-06-04 01:49:36 -0300

    Update base path to ZarpadoFit

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Zarpado-Fit">
+    <BrowserRouter basename="/ZarpadoFit">
       <App />
     </BrowserRouter>
   </React.StrictMode>
